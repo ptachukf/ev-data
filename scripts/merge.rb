5595887426bc8b823241e1dfb940b6831aeaed7a
Tijs Teulings <hello@tijs.org>
2024-06-14 13:43:23 +0200

    fix merge script and redo data merge

--- scripts/merge.rb
+++ scripts/merge.rb
@@ -1,41 +1,17 @@
 require 'multi_json'
-require 'optparse'
 
-options = {}
-OptionParser.new do |opts|
-  opts.banner = "Usage: ruby script.rb [options]"
-
-  opts.on("-f1", "--file1 FILE1", "Path to the first JSON file") do |file1|
-    options[:file1] = file1
-  end
-
-  opts.on("-f2", "--file2 FILE2", "Path to the second JSON file") do |file2|
-    options[:file2] = file2
-  end
-
-  opts.on("-o", "--output FILE", "Path to save the merged JSON file") do |output|
-    options[:output] = output
-  end
-
-  opts.on("-h", "--help", "Prints this help") do
-    puts opts
-    exit
-  end
-end.parse!
-
-if options[:file1].nil? || options[:file2].nil?
-  puts "Please provide both file paths using the -f1 and -f2 options."
+if ARGV.length != 3
+  puts "Usage: ruby merge.rb <file1> <file2> <output>"
   exit 1
 end
 
-if options[:output].nil?
-  puts "Please provide the output file path using the -o option."
-  exit 1
-end
+file1 = ARGV[0]
+file2 = ARGV[1]
+output = ARGV[2]
 
 # Load JSON files
-data1 = MultiJson.load(File.read(options[:file1]))
-data2 = MultiJson.load(File.read(options[:file2]))
+data1 = MultiJson.load(File.read(file1))
+data2 = MultiJson.load(File.read(file2))
 
 # Merge 'data' arrays, avoiding duplicates
 merged_data = data1['data'].map { |item| [item['id'], item] }.to_h
@@ -56,17 +32,14 @@ data2.fetch('brands', []).each do |item|
   merged_brands[item['id']] ||= item
 end
 
-merged_meta = data1.fetch('meta', []).map { |item| [item['key'], item] }.to_h
-data2.fetch('meta', []).each do |item|
-  merged_meta[item['key']] ||= item
-end
+merged_meta = data1.fetch('meta', {}).merge(data2.fetch('meta', {}))
 
 # Create the final merged JSON structure
 merged_result = {
   'data' => merged_data.values,
   'brands' => merged_brands.values,
-  'meta' => merged_meta.values
+  'meta' => merged_meta
 }
 
 # Save merged data to a new JSON file
-File.write(options[:output], MultiJson.dump(merged_result, pretty: true))
+File.write(output, MultiJson.dump(merged_result, pretty: true))
